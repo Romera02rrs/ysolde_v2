e1ac0b895328976d4640c05eb75f4ab1328d4398
Romera02rrs <romera02rrs@gmail.com>
2024-09-03 16:05:43 +0200

    Mic bug fix

--- app/api/speech-to-text/route.js
+++ app/api/speech-to-text/route.js
@@ -1,5 +1,5 @@
-import { NextResponse } from 'next/server';
-import OpenAI from 'openai';
+import { NextResponse } from "next/server";
+import OpenAI from "openai";
 
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
@@ -9,35 +9,39 @@ export async function POST(request) {
   try {
     // Leer los datos del archivo del request
     const formData = await request.formData();
-    const file = formData.get('file');
+    const file = formData.get("file");
 
     if (!file) {
-      return NextResponse.json({ error: 'No file uploaded' }, { status: 400 });
+      return NextResponse.json({ error: "No file uploaded" }, { status: 400 });
     }
 
-    console.log('File type: ', file.type)
+    console.log("File type: ", file.type);
 
-    let transcription = ""
+    let transcription = "";
 
-    if (file.type == "audio/mp4"){
-      const blob = new Blob([file], { type: 'mp4' });
-      const newFile = new File([blob], 'audio.mp4', { type: 'mp4' });
+    if (file.type == "audio/mp4") {
+      const arrayBuffer = await file.arrayBuffer();
+      const newBlob = new Blob([arrayBuffer], { type: file.type });
+      const newFile = new File([newBlob], file.name, { type: file.type });
       transcription = await openai.audio.transcriptions.create({
         file: newFile,
-        model: 'whisper-1'
+        model: "whisper-1",
       });
-    }else{
+    } else {
       transcription = await openai.audio.transcriptions.create({
         file: file,
-        model: 'whisper-1'
+        model: "whisper-1",
       });
     }
 
-    console.log('Transcription: ', transcription);
+    console.log("Transcription: ", transcription);
 
     return NextResponse.json({ transcription: transcription.text });
   } catch (error) {
-    console.error('Error in transcription:', error);
-    return NextResponse.json({ error: 'Error in transcription' }, { status: 500 });
+    console.error("Error in transcription:", error);
+    return NextResponse.json(
+      { error: "Error in transcription" },
+      { status: 500 }
+    );
   }
 }
